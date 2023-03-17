520edbce0d91fa501cbe81ea8148a2502863ba7b
李文强 <475860681@qq.com>
2023-03-17 19:01:56 +0800

    ```

---
+++ projectApi/Project.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Web.Controllers
+{
+ 
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("1231232");
+            return View();
+        }
+    }
+}
